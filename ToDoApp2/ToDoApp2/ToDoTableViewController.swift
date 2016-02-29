6b1e3f5e9d4c5e9d4fac873346d75c741ae070bf
Sophie Cooper <sophie@cooperzone.net>
2016-02-29 15:20:59 -0800

    cells showing

--- ToDoApp2/ToDoApp2/ToDoTableViewController.swift
+++ ToDoApp2/ToDoApp2/ToDoTableViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ToDoTableViewController: UITableViewController {
+    
+    
+    let model = [" ", " hellolsdlaksjdlkajskldjlkasjdlkajsdkjaskdjlaksjdlajsd ", "h1"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +25,19 @@ class ToDoTableViewController: UITableViewController {
     
     @IBAction func UnwindToToDoTableViewController(segue : UIStoryboardSegue) {
         
+    }
+    
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ToDoCell", forIndexPath: indexPath) as! ToDoTableViewCell
+        cell.ToDoTextLabel.text = model[indexPath.row]
+        return cell
+    }
+    
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return model.count
     }
 
 
