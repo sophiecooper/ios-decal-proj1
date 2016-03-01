79bad7d8d6e52bf1952c233ca047a26341bdfbcf
Sophie Cooper <sophie@cooperzone.net>
2016-02-29 16:39:07 -0800

    stats and add views partway done

--- ToDoApp2/ToDoApp2/ToDoTableViewController.swift
+++ ToDoApp2/ToDoApp2/ToDoTableViewController.swift
@@ -11,7 +11,12 @@ import UIKit
 class ToDoTableViewController: UITableViewController {
     
     
+    
+    
+    
     let model = [" ", " hellolsdlaksjdlkajskldjlkasjdlkajsdkjaskdjlaksjdlajsd ", "h1"]
+    
+    var CompletedTasks = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,18 +31,32 @@ class ToDoTableViewController: UITableViewController {
     @IBAction func UnwindToToDoTableViewController(segue : UIStoryboardSegue) {
         
     }
-    
 
-    
+    //show items in tableView
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("ToDoCell", forIndexPath: indexPath) as! ToDoTableViewCell
         cell.ToDoTextLabel.text = model[indexPath.row]
         return cell
     }
     
-    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return model.count
+    }
+    
+    
+    //used to delete actions
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if (editingStyle == UITableViewCellEditingStyle.Delete) {
+            //update number of completed tasks, remove from model
+            //view.reload()?
+            CompletedTasks += 1
+            
+            
+        }
     }
 
 
